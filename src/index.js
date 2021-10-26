2b97a23f43cf8740aa638af61078914e058e5514
josephkem <josephkemoy@gmail.com>
2021-10-26 18:49:34 -0400

    Started to build API

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const auth = require("./routes/auth");
+
+const app = express();
+
+app.use(auth);
+
+const mongoUri =
+  "mongodb+srv://alex91:92631043@devcon.vu3ve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on("connected", () => {
+  console.log("Database connected");
+});
+
+mongoose.connection.on("error", () => {
+  console.log("Error connecting to database");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello there!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
